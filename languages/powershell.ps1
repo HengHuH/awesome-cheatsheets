94dc34c63d2bdd37bb7f538b1b57ba3e504cd749
huangxing03 <huangxing03@corp.netease.com>
2020-09-18 15:46:15 +0800

    powershell cheatsheet

--- languages/powershell.ps1
+++ languages/powershell.ps1
@@ -3,9 +3,26 @@
 
 
 #-------------------------------------------------------------------------------------------#
+## 查看帮助
+
+Get-Help
+Get-Command
+
+
+#-------------------------------------------------------------------------------------------#
 ## 常量
+
 $PSVersionTable         # 版本信息
 $PROFILE                # 配置文件
+
+
+#-------------------------------------------------------------------------------------------#
+## 符号
+
+'|'         # 管道
+'$'         # 自定义变量 $Name
+'>'         # 重定向，覆盖文件  "Hello World!" > test.txt
+'>>'        # 重定向，添加     "Hello World!" >> log.txt
 
 
 #-------------------------------------------------------------------------------------------#
@@ -13,16 +30,8 @@ $PROFILE                # 配置文件
 
 ### 输入
 Write-Output "Hello World!" >> /.log.txt    # alias: write, echo
-### 获取指定位置项目的内容
-Get-Content ./log.txt   # alias: gc, cat, type
-
-
-#-------------------------------------------------------------------------------------------#
-## 查看帮助
-
-Get-Help
-Get-Command
-
+### 控制台输入
+$Age = Read-Host "Please enter your age" -AsSecureString
 
 #-------------------------------------------------------------------------------------------#
 ## 别名
@@ -36,11 +45,12 @@ Remove-Alias -Name del
 
 
 #-------------------------------------------------------------------------------------------#
-## 文件系统
+## 文件管理
 
 ### 获取当前工作路径
 Get-Location    # alias: pwd, gl
 ### 指定当前工作路径
+Set-Location -Path D: -PassThru
 Set-Location ./ # alias: cd
 ### 删除
 Remove-Item ./log.txt   # alias: ri, rm, rmdir, del, erase, rd
@@ -50,9 +60,13 @@ Copy-Item "./log.txt" -Destination "../log.txt" # alias: cpi, cp, copy
 Move-Item -Path ./log.txt -Destination ../log.txt   # alias: mi, mv, move
 ### 重命名
 Rename-Item -Path "./log.txt" -NewName "new_log.txt"    # alias: rni, ren
-
-
-## 列出指定目录
+### 获取指定位置项目的内容
+Get-Content ./log.txt   # alias: gc, cat, type
+### 添加内容
+Add-Content -Path .\log.txt -Value "Hello World!"   # alias: ac
+### 清除内容，但不删除 item
+Clear-Content ".\log.txt"   # alias: clc
+### 获取指定路径的子条目 
 Get-ChildItem .  # alias: ll, gci, dir
 
 function ListDirectory {
@@ -69,3 +83,14 @@ Find-Module -Name PowerShellGet # alias: fimo
 ### 安装模块
 Install-Module -Name PowerShellGet  # alias: inmo
 
+
+#-------------------------------------------------------------------------------------------#
+## 流程控制
+
+if ()
+{
+    # xx
+} else 
+{
+    # xx
+}
